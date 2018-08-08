ce7b1d18a46171ca39e893b1e1fe8799e4b2a961
Tsing <215859808@qq.com>
2018-08-08 23:26:29 +0800

    Initial Commit

---
+++ GetTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetTutorial
+//
+//  Created by Mier on 2018/8/8.
+//  Copyright © 2018年 tsing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
